18ded97fb31636e2b968dc3fcbac927826504513
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-03-01 21:30:58 -0300

    melhorias na configuração da lib ScrollReveal

--- js/main.js
+++ js/main.js
@@ -1,68 +1,68 @@
 
 //let containerElementos = document.querySelectorAll('.contato__elementos');
 //let padding = window.getComputedStyle(containerElementos).padding;
-window.scroll = ScrollReveal({reset: true, interval: 100, integrity: true} );
+const scroll = ScrollReveal({
+  reset: true,
+  interval: 100,
+  mobile: true, // Ativar configurações para dispositivos móveis
+  delay: 300, // Adicionar um pequeno atraso para melhorar a experiência do usuário
+  easing: 'ease-in-out',
+  scale: 0.95, // Diminuir o scale para melhorar a experiência do usuário
+  distance: '10px' // Diminuir a distância para melhorar a experiência do usuário
+});
 
 scroll.reveal('.navbar', {
   duration: 1400,
-  scale: '0.99',
-  easing: 'ease-in-out'
+  scale: 1,
 });
 
 scroll.reveal('.apresentacao__titulo', {
   duration: 1200,
-  distance: '100px',
+  distance: '20px',
   origin: 'left',
-  easing: 'ease-in-out'
 });
 
 scroll.reveal('.apresentacao__lista', {
   duration: 1000,
-  distance: '100px',
+  distance: '20px',
   origin: 'left',
-  easing: 'ease-in-out'
 });
 
 scroll.reveal('.apresentacao__banner', {
   duration: 1300,
-  distance: '100px',
+  distance: '20px',
   origin: 'right',
-  easing: 'ease-in-out'
 });
 
 scroll.reveal('.sobre__mim', {
   duration: 1000,
-  distance: '100px',
+  distance: '20px',
   origin: 'top',
-  easing: 'ease-in-out'
 });
-  
+
 scroll.reveal('.skills__card', {
-  mobile: true,
   duration: 1000,
-  distance: '100px',
+  distance: '20px',
   origin: 'left',
-  easing: 'ease-in-out'
 });
 
 scroll.reveal('.hobbies__card', {
   duration: 1000,
-  distance: '100px',
+  distance: '20px',
   origin: 'right',
-  easing: 'ease-in-out'
 });
 
 scroll.reveal('.formacao', {
   duration: 800,
-  distance: '100px',
+  distance: '20px',
   origin: 'bottom',
-  easing: 'ease-in-out'
 });
 
 scroll.reveal('.contato', {
   duration: 1200,
-  opacity: 0.1
+  opacity: 0.1,
 });
+
  
 let inputs = document.querySelectorAll('.contato__input');
 let textarea = document.querySelector('.contato__input--textarea');
